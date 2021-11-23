ed3fb56d5294266bb219721a24f9fba990cdf367
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-11-23 10:53:55 -0300

    Listagem básica funcionando

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Categoria, Gasto
 # Create your views here.
+
+# GET lista todos gastos cadastrados
+def listaGastos(request):
+    gastos = Gasto.objects.all()
+    return render(request, 'index.html', {'gastos': gastos})
+
