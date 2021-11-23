285aff3bb5b762a48003ebb6a506ec8a7a022417
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-11-23 14:31:41 -0300

    Cadastro simples está pronto

--- finance/urls.py
+++ finance/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.listaGastos, name="gastos" ),
-    path('cadastrar', views.cadastrarGasto, name="cadastrar_gastos" )
+    path('cadastrar', views.cadastrarGasto, name="cadastrar_gastos" ),
+    path('categoria', views.cadastraCategoria, name="cadastrar_categorias" )
 ]
